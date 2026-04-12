cc46b8af292264167a4bd0afe6f18ee617c6d696
Samruddhi Vinod Yerekar <samruddhiyerekar3@gmail.com>
2026-04-12 22:03:30 +0530

    Add Day31_StringLength.java to calculate string length

---
+++ Day31_StringLength.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Day31_StringLength {
+
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+
+        
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+
+      
+        int length = input.length();
+
+        
+        System.out.println("Length of the string: " + length);
+
+        scanner.close();
+    }
+}

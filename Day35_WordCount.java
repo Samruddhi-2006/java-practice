092986dee43a949c2ba0bbdfd74fe27eb4b667ac
Samruddhi Vinod Yerekar <samruddhiyerekar3@gmail.com>
2026-04-16 23:52:08 +0530

    Add word count functionality in Day35_WordCount.java

---
+++ Day35_WordCount.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Day35_WordCount {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a sentence: ");
+        String sentence = sc.nextLine();
+
+      
+        String[] words = sentence.trim().split("\\s+");
+
+        int count = words.length;
+
+        System.out.println("Number of words: " + count);
+
+        sc.close();
+    }
+}
